833e6734bb6bd79d659ac3b96e9d2e529b47b78c
nazar.khazratov <nazar.khazratov@stryker.com>
2022-05-20 21:07:28 +0300

    test

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -43,6 +43,7 @@ namespace WHApp_API.Controllers
                     // case typeof(TypeLoadException):
                     //     return;
                 }
+                
             }
             return Ok();
         }
